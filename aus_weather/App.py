4cc99f431302a74015107104b9d8569427ef05e2
jherberg462 <54086086+jherberg462@users.noreply.github.com>
2020-03-05 23:14:25 -0500

    Update App.py

--- aus_weather/App.py
+++ aus_weather/App.py
@@ -161,4 +161,3 @@ if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
->>>>>>> heroku-development
